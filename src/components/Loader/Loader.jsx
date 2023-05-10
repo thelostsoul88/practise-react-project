5b1550b4a53f29add3892ed5c8a14cb0681e9e45
thelostsoul88 <demon.com54@gmail.com>
2023-05-10 20:37:42 +0300

    update loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,21 +1,16 @@
-import { ColorRing } from "react-loader-spinner";
-import Backdrop from "@mui/material/Backdrop";
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 const Loader = () => {
   return (
     <div>
       <Backdrop
-        sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+        sx={{ color: '#fff', zIndex: theme => theme.zIndex.drawer + 1 }}
         open={true}
       >
-        <ColorRing
-          visible={true}
-          height="120"
-          width="120"
-          ariaLabel="blocks-loading"
-          wrapperStyle={{}}
-          wrapperClass="blocks-wrapper"
-          colors={["#b8c480", "#B2A3B5", "#F4442E", "#51E5FF", "#429EA6"]}
-        />
+        <Box sx={{ display: 'flex' }}>
+          <CircularProgress />
+        </Box>
       </Backdrop>
     </div>
   );
